e6e9ddcd0be32c5665eb079223c849e1ea39c0bc
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-07-19 16:17:05 -0400

    created a find by id route in tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -17,9 +17,17 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try{  
+    const singleTag = await Tag.findByPk(req.params.id,{
+      include:[{model: Product, through: ProductTag}],
+    });
+    res.status(200).json(singleTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
