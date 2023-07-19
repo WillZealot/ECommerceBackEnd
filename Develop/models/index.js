26ecbe10cfadf3a1cd08dae33d34747d4275ceee
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-07-18 21:16:17 -0400

    added in files code to associate models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -11,6 +11,21 @@ const ProductTag = require('./ProductTag');
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey : 'category_id',
+});
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey : 'category_id',
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: 'tag_id' });
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: 'product_id' });
+
+
 
 module.exports = {
   Product,
